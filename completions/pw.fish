a8ea5c8ad9119a759c92a4790a9e21fcba11f1cc
w40141 <w.40141@gmail.com>
2020-06-23 16:59:42 +0900

    add delete_all

--- completions/pw.fish
+++ completions/pw.fish
@@ -6,4 +6,5 @@ complete -f -c pw -n '__fish_is_subcommand' -a generate -d "generate password fr
 complete -f -c pw -n '__fish_is_subcommand' -a show -d "show a password of domain"
 complete -f -c pw -n '__fish_is_subcommand' -a show_all -d "show all passwords"
 complete -f -c pw -n '__fish_is_subcommand' -a delete -d "delete a password"
+complete -f -c pw -n '__fish_is_subcommand' -a delete_all -d "delete all passwords"
 complete -f -c pw -n '__fish_is_subcommand' -a change -d "change a password"
